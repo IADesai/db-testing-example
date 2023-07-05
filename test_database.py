eb0e30234445ce74a3923eeea0c32a6de1824d31
Ibrahim Ayub Desai <113534999+IADesai@users.noreply.github.com>
2023-07-05 12:41:12 +0100

    Workshop

--- test_database.py
+++ test_database.py
@@ -1,6 +1,8 @@
+import pytest
+
 from unittest.mock import MagicMock
 
-from database import get_all_messages, get_all_messages_no_with
+from database import get_all_messages, get_all_messages_no_with, insert_message
 
 
 def test_get_all_messages_returns_list():
@@ -29,3 +31,32 @@ def test_get_all_messages_no_with_returns_list():
     assert fake_fetch.call_count == 1
     assert fake_execute.call_count == 1
     assert fake_execute.call_args[0] == ("SELECT * FROM message;",)
+
+
+def test_insert():
+    fake_connection = MagicMock()
+
+    fake_execute = fake_connection.cursor().__enter__().execute
+    fake_fetch = fake_connection.cursor().__enter__().fetchone
+    fake_fetch.return_value = ()
+    arg = "Argument"
+    result = insert_message(fake_connection, arg)
+
+    assert isinstance(result, tuple)
+    assert fake_fetch.call_count == 1
+    assert fake_execute.call_count == 1
+    assert fake_execute.call_args[0] == ("""
+                    INSERT INTO message (message_text)
+                    VALUES (%s)
+                    RETURNING *;
+                    """, [arg])
+
+
+# def test_insert_raise_Error():
+#     fake_connection = MagicMock()
+
+#     arg = []
+#     result = insert_message(fake_connection, arg)
+
+#     with pytest.raises(TypeError):
+#         Message text must be a string
